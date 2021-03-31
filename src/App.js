5548fa60e132908775e79e4b2eea25a8c070ea0a
Luthfi Khusyasy <Khusyasy@users.noreply.github.com>
2021-03-31 21:47:28 +0700

    base ui design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 
 function App() {
     return (
-        <div>
-            <p>HELLO</p>
+        <div className="main-container center-all">
+            <div className="main-paper center-all">
+                <h3 className="quotes">HELLO</h3>
+                <p className="author">HELLO</p>
+                <div className="btn-group">
+                    <button className="btn">Random Quotes</button>
+                    <button className="btn">Share Quotes</button>
+                </div>
+            </div>
         </div>
     );
 }
